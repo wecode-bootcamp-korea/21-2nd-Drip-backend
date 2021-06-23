c34a173b645f8bdd24f774f79ff79e3e63f38f0d
redsaumon <51191484+redsaumon@users.noreply.github.com>
2021-06-23 15:00:54 +0900

    Revert "Add: login decorator"

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,3 @@
-from pathlib import Path
 from django.urls import path
 
-from users.views import KakaoSigninView
-
-urlpatterns = [
-    path('kakao', KakaoSigninView.as_view()),
-]
+urlpatterns = []

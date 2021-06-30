68ddbac8c7caf4dfac014e166b4a6ec32b4978f7
박준영 <sjwm98@gmail.com>
2021-06-30 18:51:16 +0900

    Add:리뷰작성구현, 유닛테스트 완료:

--- drip/urls.py
+++ drip/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('users', include('users.urls')),
     # path('products', include('products.urls')),
     # path('orders', include('orders.urls')),
+    path('reviews', include('reviews.urls'))
 ]
---
+++ reviews/views.py
@@ -0,0 +1,81 @@
+import boto3
+
+from logging                     import log
+from django.db.models.aggregates import Count
+from django.views                import View
+from django.http                 import JsonResponse
+from datetime                    import datetime
+
+from drip_settings   import ACCESS_KEY, SECRET_ACCESS_KEY
+from users.utils     import login_decorator
+from .models         import Like, Review, Comment
+from products.models import Product
+
+class ReviewView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            user    = request.user
+            content = request.POST['content']
+            rating  = request.POST['rating']
+            product = Product.objects.get(id=request.POST['product'])
+            
+            s3_client = boto3.client(
+                's3',
+                aws_access_key_id     = ACCESS_KEY,
+                aws_secret_access_key = SECRET_ACCESS_KEY
+            )
+            
+            image      = request.FILES['filename']
+            image_time = (str(datetime.now())).replace(" ","")
+            image_type = (image.content_type).split("/")[1]                                     
+            
+            s3_client.upload_fileobj(
+                image,
+                "dripawsbucket21",
+                image_time+"."+image_type,
+                ExtraArgs = {
+                    "ContentType" : image.content_type
+                }
+            )
+
+            image_url = "https://dripawsbucket21.s3.ap-northeast-2.amazonaws.com/"+image_time+"."+image_type
+
+            Review.objects.create(
+                user      = user,
+                content   = content,
+                rating    = rating,
+                image_url = image_url if image else None,
+                product   = product,
+            )
+            return JsonResponse({'result' : 'SUCCESS'}, status = 201)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+    @login_decorator
+    def get(self, request, product_id):
+        user    = request.user
+        order   = request.GET.get('order')
+
+        order_conditions = {
+            'new' : '-created_at',
+            'old' : 'created_at',
+            'like' : '-like_count',
+            'unlike' : 'like_count',
+        }
+
+        reviews  = Review.objects.filter(product_id=product_id).annotate(like_count = Count('like')).order_by(order_conditions.get(order, '-created_at'))
+            
+        review_list = [{
+            'created_at'   : review.created_at,
+            'user'         : review.user.id,
+            'content'      : review.content,
+            'rating'       : review.rating,
+            'product'      : product_id,
+            'like_count'   : review.like_count,
+            'review_count' : reviews.count(),
+            'image_url'    : review.image_url,
+            'like'         : Like.objects.filter(user_id = user.id, review_id = review.id).exists()
+            } for review in reviews]
+        return JsonResponse({'result' : review_list}, status = 200)

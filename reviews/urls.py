ab149b8eeccbb0b74c64e352a4782c22c247d62a
박준영 <sjwm98@gmail.com>
2021-07-01 21:30:44 +0900

    ADD:s

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ReviewView, CommentView
+from .views import ReviewView, CommentView, LikeView
 
 urlpatterns = [
     path('', ReviewView.as_view()),
     path('/<int:product_id>', ReviewView.as_view()),
     path('/comment', CommentView.as_view()),
-    path('/comment/<int:review_id>', CommentView.as_view())
+    path('/comment/<int:review_id>', CommentView.as_view()),
+    path('/like', LikeView.as_view()),
+    path('/like/<int:review_id>', LikeView.as_view())
 ]

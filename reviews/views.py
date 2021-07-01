b9e73290a0a6387f1c018ad0cae6f3c7f5c9b881
박준영 <sjwm98@gmail.com>
2021-07-01 19:14:04 +0900

    Add:리뷰좋아요 구현, 유닛테스트 완료

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-import boto3
+import boto3, json
 
 from logging                     import log
 from django.db.models.aggregates import Count
@@ -8,7 +8,7 @@ from datetime                    import datetime
 
 from drip_settings   import ACCESS_KEY, SECRET_ACCESS_KEY
 from users.utils     import login_decorator
-from .models         import Like, Review, Comment
+from .models         import Like, Review
 from products.models import Product
 
 class ReviewView(View):
@@ -76,6 +76,29 @@ class ReviewView(View):
             'like_count'   : review.like_count,
             'review_count' : reviews.count(),
             'image_url'    : review.image_url,
-            'like'         : Like.objects.filter(user_id = user.id, review_id = review.id).exists()
+            'like'         : Like.objects.filter(user_id = user.id, review_id = review.id).exists(),
+            'user_image'   : review.user.profile_image
             } for review in reviews]
         return JsonResponse({'result' : review_list}, status = 200)
+
+class LikeView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data      = json.loads(request.body)
+            user      = request.user
+            review_id = data['review_id']
+
+            if not Review.objects.filter(id = review_id).exists():
+                return JsonResponse({'message' : 'INVALID_REVIEW'}, status = 401)
+
+            if not Like.objects.filter(user = user, review_id = review_id).exists():
+                Like.objects.create(user = user, review_id = review_id)
+                return JsonResponse({'message' : 'SUCCESS'}, status= 201)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+    def delete(self, request, review_id):
+        Like.objects.filter(user = request.user, review_id = review_id).delete()
+        return JsonResponse({'message' : 'SUCCESS'}, status = 204)
